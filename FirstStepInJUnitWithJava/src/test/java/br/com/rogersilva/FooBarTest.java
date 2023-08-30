9643d4f51fbe2559c7bee1a5c1c54d5d6d5ed841
Roger Silva <rogersilvati@gmail.com>
2023-08-30 15:37:23 -0300

    S303 Criado o primeiro teste

---
+++ FirstStepInJUnitWithJava/src/test/java/br/com/rogersilva/FooBarTest.java
@@ -0,0 +1,14 @@
+package br.com.rogersilva;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FooBarTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
