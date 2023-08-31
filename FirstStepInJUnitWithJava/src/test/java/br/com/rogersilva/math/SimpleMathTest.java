d8867b28319d189de591ede8581466696ff57ccb
Roger Silva <rogersilvati@gmail.com>
2023-08-31 14:27:19 -0300

    S309 Aprimorando o codigo

--- FirstStepInJUnitWithJava/src/test/java/br/com/rogersilva/math/SimpleMathTest.java
+++ FirstStepInJUnitWithJava/src/test/java/br/com/rogersilva/math/SimpleMathTest.java
@@ -8,8 +8,11 @@ public class SimpleMathTest {
 	@Test
 	void testSum() {
 		SimpleMath math = new SimpleMath();
-		Double result = math.sum(6.2D, 2D);
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		Double result = math.sum(firstNumber, secondNumber);
 		double expected = 8.2D;
+		
 		Assertions.assertEquals(expected, result, "The testSum() did not produce expected result");
 	}
 
